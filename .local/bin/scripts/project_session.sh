fe55c5aa5f3615ecdb7d4b6e57d047d877b6bb56
Chenxin Yan (Cyan) <cy2558@nyu.edu>
2024-09-29 01:47:33 -0400

    update tmux script

--- .local/bin/scripts/project_session.sh
+++ .local/bin/scripts/project_session.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Tmux Cleanup Sessions
+tmux list-sessions -F "#{session_name}" | while read -r session_name; do
+  DEV_PATH="$DEV_DIR/$(echo "$session_name" | tr '_' '.')"
+  PROJECT_PATH="$PROJECT_DIR/$(echo "$session_name" | tr '_' '.')"
+  if [ ! -d "$DEV_PATH" ] && [ ! -L "$DEV_PATH" ] && [ ! -d "$PROJECT_PATH" ] && [ ! -L "$PROJECT_PATH" ]; then
+    echo "Killing tmux session: $session_name (Folder does not exist)"
+    tmux kill-session -t "$session_name"
+  fi
+done
+
 # Check if the project directory exists, if not, create it
 if [ ! -d "$PROJECT_DIR" ]; then
   mkdir -p "$PROJECT_DIR"
